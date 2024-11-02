3746d680b0ad4bb5730f8f6c9ad92ed432b451ba
JohnDoe-Jack <makaroni.aori0421@gmail.com>
2024-11-02 17:41:08 +0900

    WIP

--- 02/print.h
+++ 02/print.h
@@ -2,19 +2,3 @@
 #include <stdlib.h>
 
 #include "util.h"
-
-/**
- * @brief 初期化関数
- * プリティプリントを行う前に呼び出す
- * @param filename 
- * @return FILE* 
- */
-FILE * initScan(char * filename)
-{
-  FILE * fin;
-  if ((fin = fopen(filename, "r")) == NULL) {
-    error("File not found.");
-    exit(1);
-  }
-  return fin;
-}
