497e516e57f22fff82459ee43e07803c717cebf8
JohnDoe-Jack <makaroni.aori0421@gmail.com>
2024-11-22 09:25:12 +0900

    Add test

---
+++ 02/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+rm *.gcda *.gcno *.o *.gcov pp
+
+gcc -coverage -c *.c *h
+gcc -coverage -o pp *.o
+
+for file in ../test/*.mpl; do
+  if [ -f "$file" ]; then
+    echo "Processing $file..."
+    ./pp "$file"
+  else
+    echo "No .mpl files found in test directory."
+  fi
+done
+
+./pp
+./pp test/hoge.mpl
+
+gcov -b *.gcda
